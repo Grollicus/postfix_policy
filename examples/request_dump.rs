1f402ab1c0aff66001ea980b0420c13b0b49312b
Grollicus <Grollicus@users.noreply.github.com>
2019-05-26 12:19:26 +0200

    added request_dump example

---
+++ examples/request_dump.rs
@@ -0,0 +1,47 @@
+use postfix_policy::{PolicyRequestHandler, PolicyResponse, handle_connection};
+use std::string::String;
+use std::os::unix::net::UnixListener;
+use std::thread;
+use std::io::{BufReader, Write};
+use std::fs::remove_file;
+use std::io::stdout;
+
+struct RequestDumper<'l> {
+    connection_number: &'l usize,
+    output: String,
+}
+
+impl<'l> PolicyRequestHandler<'l, usize> for RequestDumper<'l> {
+    fn new(connection_number: &'l usize) -> Self { Self{
+        connection_number: connection_number,
+        output: String::new(),
+    }}
+    fn attribute(&mut self, name: &[u8], value: &[u8]) {
+        self.output.push_str(&format!("{}={}\n", String::from_utf8_lossy(name), String::from_utf8_lossy(value)));
+    }
+    fn response(self) -> PolicyResponse {
+        let stdout_mutex = stdout();
+        let mut stdout = stdout_mutex.lock();
+        writeln!(stdout, "Request on Connection #{}", self.connection_number).expect("Writing to stdout failed");
+        write!(stdout, "{}", self.output).expect("Writing to stdout failed");
+        writeln!(stdout, "End of Request on Connection #{}", self.connection_number).expect("Writing to stdout failed");
+        PolicyResponse::Dunno
+    }
+}
+
+fn main() {
+    remove_file("/tmp/policy_example").ok();
+    let listener = UnixListener::bind("/tmp/policy_example").expect("Binding listener socket failed");
+
+    let mut connection_count: usize = 0;
+    for client in listener.incoming() {
+        let connection_number = connection_count;
+        connection_count += 1;
+        thread::spawn(move || {
+            let client = client.expect("Something failed while listening");
+            let mut sender = client.try_clone().expect("Could not clone client socket");
+            let receiver = BufReader::new(client);
+            handle_connection::<RequestDumper, _, _, _>(receiver, &mut sender, &connection_number).expect("handling connection failed");
+        });
+    }
+}
